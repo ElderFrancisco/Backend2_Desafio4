578279303f310f6d8b2affe806d5ea33e310115d
Francisco <franciscoelder0@gmail.com>
2023-10-11 23:11:01 -0300

    agrego por form

--- src/util/socket.js
+++ src/util/socket.js
@@ -1,14 +1,19 @@
-const http = require('http');
 const socketIO = require('socket.io');
 
 const ProductManager = require('../components/products/productController/productManager');
 const productController = new ProductManager('../../../../productos.json');
 
 module.exports = (server) => {
-  console.log('aca');
   const io = socketIO(server);
+
   io.on('connection', async (socket) => {
     console.log('Cliente conectado');
+
     socket.emit('productos', await productController.getProducts());
+
+    socket.on('newProduct', async (product) => {
+      const newProduct = await productController.addProduct(product);
+      const caca = await productController.getProducts();
+    });
   });
 };
